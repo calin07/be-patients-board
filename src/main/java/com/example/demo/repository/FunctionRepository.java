db0ec2d6717d2c448862384d27a2d4423e656e7d
Sabina Radu <radu.sabina00@yahoo.ca>
2023-04-26 09:24:06 +0300

    split the functions folder

--- src/main/java/com/example/demo/repository/FunctionRepository.java
+++ src/main/java/com/example/demo/repository/FunctionRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.Function;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FunctionRepository extends JpaRepository<Function,Long> {
+public interface FunctionRepository extends JpaRepository<Function, Long> {
 }
